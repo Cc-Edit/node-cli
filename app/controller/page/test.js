b33494916a7d22b96d212227437fa5d87e7043fd
wenqiang.li <wenqiang.li@renren-inc.com>
2020-02-14 21:52:27 +0800

    vue-ssr

--- app/controller/page/test.js
+++ app/controller/page/test.js
@@ -1,4 +1,4 @@
 exports.home = function* (ctx) {
-  // {app_root}/app/view/vue-ssr-server-bundle.js
-  yield ctx.render('vue-ssr-server-bundle.js', { name: 'vue js bundle render' });
+  // {app_root}/app/view/vue-ssr-server-bundle.json
+  yield ctx.render('vue-ssr-server-bundle.json', { name: 'vue json render' });
 };
